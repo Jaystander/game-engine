a1a1ca4fccd376f4ed49ce4a7d6b6673dceb0dff
Jaystander <Jaystander@hotmail.com>
2015-12-30 21:19:27 -0800

    Update openingscene.py

--- Scenario/openingscene.py
+++ Scenario/openingscene.py
@@ -1,3 +1,12 @@
 import scene
+import Objects
 
 class OpeningScene(scene):
+    master_list = []
+    objects = []
+    object_descriptions = []
+    places = []
+    place_descriptions = []
+    npcs = []
+    npc_descriptions = []
+    description = 'This string describes the scene'
