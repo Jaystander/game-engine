eca2dc06154efd9aefbdb88f9939aeb9bf34dc24
Jaystander <Jaystander@hotmail.com>
2015-12-30 21:12:30 -0800

    Update and rename object.py to base.py

---
+++ Objects/base.py
@@ -0,0 +1,114 @@
+import main
+
+class Object():
+    description = 'This is an empty object'
+    can_talk = False
+    can_look = False
+    can_grab = False
+    can_touch = False
+    can_use = False
+    can_go = False
+    type = 'object' # npc and place are also possible.
+    
+    def Talk(self): # if the actions are possible define what they do.
+        pass
+        
+    def Use(self):
+        pass
+        
+    def Grab(self):
+        pass
+        
+    def Touch(self):
+        pass
+        
+    def Look(self):
+        pass
+    
+    def Go(self):
+        pass
+
+class NPC():
+    
+    description = 'This is an empty object'
+    can_talk = False
+    can_look = False
+    can_grab = False
+    can_touch = False
+    can_use = False
+    can_go = False
+    type = 'npc' # npc and place are also possible.
+    
+    def Talk(self): # if the actions are possible define what they do.
+        pass
+        
+    def Use(self):
+        pass
+        
+    def Grab(self):
+        pass
+        
+    def Touch(self):
+        pass
+        
+    def Look(self):
+        pass
+    
+    def Go(self):
+        pass
+    
+class Item(): #base class for usable/collectable items/equipment
+    
+    description = 'This is an item'
+    equipable = False
+    usable = False
+    quantity = False
+    consumable = False
+    can_use = False
+    can_go = False
+    type = 'item' # npc and place are also possible.
+    
+    def Talk(self): # if the actions are possible define what they do.
+        pass
+        
+    def Use(self):
+        pass
+        
+    def Grab(self):
+        pass
+        
+    def Touch(self):
+        pass
+        
+    def Look(self):
+        pass
+    
+    def Go(self):
+        pass
+    
+class Transition():
+    
+    description = 'This is an empty object'
+    visible = True
+    locked = False
+    open = False
+    can_go = False
+    type = 'transition'
+    
+    def Talk(self): # if the actions are possible define what they do.
+        pass
+        
+    def Use(self):
+        pass
+        
+    def Grab(self):
+        pass
+        
+    def Touch(self):
+        pass
+        
+    def Look(self):
+        pass
+    
+    def Go(self):
+        pass
