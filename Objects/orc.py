6c2048730cee351476b931e8b0034b4b0d3b1aa4
Jaystander <Jaystander@hotmail.com>
2016-01-01 07:12:36 -0800

    Update orc.py

--- Objects/orc.py
+++ Objects/orc.py
@@ -1,6 +1,12 @@
 import base
 import main
+import Draw
 
 class Orc(base.NPC):
   can_talk = True
   can_fight = True
+  can_trade = True
+  
+  def Talk(self):
+    pass #need a drawing fuction to handle the text boxes.
+    
