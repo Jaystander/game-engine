2b3bd944c6e7daf45167ebaedc92614305ad69ec
Jaystander <Jaystander@hotmail.com>
2016-01-06 20:19:32 -0800

    Update file.py

--- Draw/file.py
+++ Draw/file.py
@@ -1,10 +1,26 @@
 import re
 
 
-def DefString(keyphrase):
-  stringrtrn = ''
+def DefString(file, keyphrase):
+  strng = ''
+  with open(file) as infile:
+    copy = False
+    for line in infile:
+        if line.strip() == keyphrase:
+            copy = True
+        elif line.strip() == "Finish":
+            copy = False
+        elif copy:
+            strng.append(line)
+  if strng == '':
+    print "String is empty"
+  else:
+    return strng
+
+
+  """stringrtrn = ''
   with open('file.txt', 'r') as searchfile:
       for line in searchfile:
           if keyphrase in line:
             stringrtrn.append(line)
-  return stringrtrn
+  return stringrtrn"""
