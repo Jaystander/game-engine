995c0ccc4b6ae9da1290f72aa322bc6540c175dc
Jaystander <Jaystander@hotmail.com>
2016-01-07 01:01:52 -0800

    Update dialogue.py

--- Draw/dialogue.py
+++ Draw/dialogue.py
@@ -1,1 +1,14 @@
+import pygame, draw
 
+dialogue_box = pygame.Rect(1,1,1,1)
+response_box = pygame.Rect(1,1,1,1)
+class Dialogue():
+  display = ''
+  response = ''
+  
+  def InitDialogue(text, responses):
+    Draw.draw.dialogue = True
+    display = text
+    response = responses
+    draw.typing(display, dialogue_box)
+    draw.typing(response, response_box)
