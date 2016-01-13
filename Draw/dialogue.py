b879ee8dcc2b82d3809a473c6074ed171fe00dcb
Jaystander <Jaystander@hotmail.com>
2016-01-13 10:23:14 -0800

    Update dialogue.py

--- Draw/dialogue.py
+++ Draw/dialogue.py
@@ -9,12 +9,25 @@ response_box = pygame.Rect(200, 20, 100, 300)
 class Dialogue():
   display = ''
   response = ''
+  response_ref = []
   
   def InitDialogue(text, responses):
     Draw.draw.dialogue = True
     display = text
     response = responses
     draw.typing(display, dialogue_box)
+    Dialogue.SetResponses()
     draw.typing(response, response_box)
     
-    
+  def SetResponses():
+    responses = response.split()
+    fullstring = ''
+    tag = False
+    for word in responses:
+      if tag == True:
+        respnse_ref.append(word)
+        tag = False
+      elif word == 'reff':
+        tag = True
+      elif word != 'reff':
+        each += '' + word
