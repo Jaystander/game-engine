b8514897f16b0e5d3cccbe0bb9e0983a4e47ef36
Jaystander <Jaystander@hotmail.com>
2016-01-13 09:47:59 -0800

    Update robotleadersoffice.py

--- Scenario/robotleadersoffice.py
+++ Scenario/robotleadersoffice.py
@@ -1,3 +1,10 @@
 import scene
+import Objects
+
+robotleader = Objects.robotleader.RobotLeader
 
 class RobotLeadersOffice(scene.Scene):
+  reference = "Robot Leader's Office"
+  masterlist = [robotleader]
+  description = 'This is the description for this scene.'
+  
